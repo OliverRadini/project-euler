3b2b32f71f41445f5f23ec0f1063058bac63f39e
Oliver Radini <orr2@live.com>
2023-04-03 22:51:34 +0100

    Add apparently working, but very slow iterative brute force solution

--- 015/015.js
+++ 015/015.js
@@ -2,24 +2,11 @@
 Starting in the top left corner of a 2×2 grid, and only being able to move
 to the right and down, there are exactly 6 routes to the bottom right corner.
 
-
 How many such routes are there through a 20×20 grid?
 */
 
-/**
- * consider this as a graph of nodes
- * 
- * there are n nodes in a row
- * there are n rows
- * each node is connected to those around it
- * 
- * We can solve this as a linear programming problem
- * 
- * We, from each node, remember the path to the end,
- * and we can just build them up from there
- */
 const nodes = [];
-const size = 3;
+const size = 5;
 
 for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
@@ -29,8 +16,7 @@ for (let x = 0; x < size; x++) {
 
 function buildEdgesForNode ({ x, y }) {
     return [
-        [x, y-1],
-        [x-1, y], [x+1, y],
+        [x+1, y],
         [x, y+1]
     ]
         .filter(([x1, y1]) => x1 >= 0 && x1 < size && y1 >= 0 && y1 < size)
@@ -51,44 +37,42 @@ const edgeLookup = edges.reduce((p, c) => {
     return p;
 }, {});
 
-/**
- * 
- * @param {Object} from with x and y properties, the from node
- * @param {Object} to  with x and y properties, the target node
- * @param {Array<Object>} visited of visited coordinates
- * @returns an array of arrays of all possible paths, represnted as coordinate objects
- */
-function buildPathsFromTo(from, to, visited = []) {
-    if (from.x === to.x && from.y === to.y) {
-        return [[to]];
+
+const foundPaths = [];
+const pathQueue = [[{ x: 0, y: 0 }]];
+while (true) {
+    if (pathQueue.length % 100 === 0) {
+        console.log(pathQueue.length);
+        console.log(foundPaths.length);
+    }
+    if (pathQueue.length === 0) {
+        break;
     }
 
-    const nextNodes = edgeLookup[`${from.x}, ${from.y}`]
-        .filter(a => !visited.some(b => a.x === b.x && a.y === b.y));
+    const workingPath = pathQueue[0];
+    pathQueue.shift();
+
+    const lastNode = workingPath[workingPath.length - 1];
+
+    if (lastNode.x === size - 1 && lastNode.y === size - 1) {
+        foundPaths.push([...workingPath, lastNode]);
+        continue;
+    }
+
+    const nextNodes = edgeLookup[`${lastNode.x}, ${lastNode.y}`]
+        .filter(a => !workingPath.some(b => a.x === b.x && a.y === b.y));
 
     if (nextNodes.length === 0) {
-        return null;
+        continue;
     }
 
-    const nextVisited = [...visited, from];
+    const nextPaths = nextNodes.map(node => [...workingPath, node]);
 
-    const nextPaths = nextNodes
-        .flatMap(node => buildPathsFromTo(node, to, nextVisited))
-        .reduce((p, c) => {
-            if (c === null) { 
-                return p;
-            }
-
-            return [...p, c];
-        }, [])
-        
-    const withFrom = nextPaths
-        .map(path => [from, ...path]);
-       
-    return withFrom;
+    for (let path of nextPaths) {
+        pathQueue.push(path);
+    }
 }
 
-const paths = buildPathsFromTo({ x: 0, y: 0}, {x: size - 1, y: size - 1});
-
-console.log(edges);
-console.log(edgeLookup);
+console.log(foundPaths.map(path => path.map(node => `(${node.x}, ${node.y})`).join(", ")));
+console.log(foundPaths.map(x => x.length));
+console.log(foundPaths.length);
