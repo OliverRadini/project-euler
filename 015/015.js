f8ac021cc507250297ebeb13cbc7888fb92c1cad
Oliver Radini <orr2@live.com>
2023-04-04 22:57:49 +0100

    Add working version of binary-number-solution; still too slow, need to look at ways to speed this up

--- 015/015.js
+++ 015/015.js
@@ -1,17 +1,20 @@
 // note that the size of the path is fixed as n + 1
 
 const size = 20;
+const sizeOfRoute = (size - 1) * 2;
 
-const max = parseInt(Array.from(Array(size + 1)).map(() => "1").join(""), 2);
+const min = parseInt(
+    Array.from(Array(sizeOfRoute / 2)).map(() => "0").join("") + Array.from(Array(sizeOfRoute / 2)).map(() => "1").join(""),
+    2
+);
+const max = parseInt(Array.from(Array(sizeOfRoute)).map(() => "1").join(""), 2);
+
+console.log(`min is ${min}, max is ${max}`);
 
 const foundPaths = [];
 let count = 0;
-for (let i = 0; i < max; i++) {
-    const inBase2 = (i >>> 0).toString(2).padStart(size + 1, "0");
-
-    if (inBase2.length !== size + 1) {
-        continue;
-    }
+for (let i = min; i < max; i++) {
+    const inBase2 = (i >>> 0).toString(2).padStart(sizeOfRoute, "0");
 
     let zeroes = 0;
     let ones = 0;
@@ -26,9 +29,7 @@ for (let i = 0; i < max; i++) {
 
     if (ones === zeroes) {
         count++;
-        foundPaths.push(inBase2);
     }
 }
 
-console.log(foundPaths);
 console.log(count);
