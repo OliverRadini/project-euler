1d177a9fe83d0b50e36b89c9b414fa885b15d48b
Oliver Radini <orr2@live.com>
2023-04-06 22:41:27 +0100

    Add attempt at solving mathematically; not really easy as it gets complicated

--- 015/015.js
+++ 015/015.js
@@ -1,36 +1,51 @@
-// note that the size of the path is fixed as n + 1
+// for a grid of size n, how many routes are added with a new layer?
 
-const size = 20;
-const sizeOfRoute = (size - 1) * 2;
+/**
+ * 
+ * Adding a layer above and to the left:
+ * 
+ *     ooo          xxxx
+ *     ooo    -->   xooo 
+ *     ooo          xooo
+ *                  xooo
+ * 
+ *    1 + 2 + 4 + 2 + 1 = 10 (for third layer added)
+ * 
+ *    1 + 2 + 3 + 6 + 3 + 2 + 1 = 18 (for the fourth layer)
+ * 
+ * 
+ * With each new layer that is added, 2n + 1 squares are added.
+ * 
+ * The corner square can go to every other square that's added.
+ * 
+ * Every other square can go to the square along its edge to the
+ * right/down of it; not both, so, for each edge:
+ * 
+ * 1 + 2 + 3 ... + (n - 1)
+ * 
+ * This is the triangular number (n-1)
+ * 
+ * This is calculated as (x^2 + x) / 2
+ * 
+ * (n-1)(n-1) = n^2 - 2n + 1
+ * 
+ * So then each edge adds:
+ * 
+ * (n^2 - n) / 2
+ * 
+ * As we have two edges, these therefore add:
+ * 
+ * (n^2 - n) routes
+ * 
+ * The corner piece adds 2n - 2 routes
+ * 
+ * So, the total number of routes added per layer is:
+ * 
+ * n^2 + n - 2
+ * 
+ * Not typically that simple though; as it depends on the square that it is connected to
+ * 
+ * You need to know the number of routes out of the block that you're connecting to
+ * 
+ */
 
-const min = parseInt(
-    Array.from(Array(sizeOfRoute / 2)).map(() => "0").join("") + Array.from(Array(sizeOfRoute / 2)).map(() => "1").join(""),
-    2
-);
-const max = parseInt(Array.from(Array(sizeOfRoute)).map(() => "1").join(""), 2);
-
-if (max > Number.MAX_SAFE_INTEGER) {
-    throw new Error("Grid is too large to process");
-}
-
-console.log(`min is ${min}, max is ${max}`);
-
-const foundPaths = [];
-let count = 0;
-for (let i = min; i < max; i++) {
-    if (i % 10000 === 0) {
-        const percent = (i / max) * 100;
-        console.log(`${percent}%`);
-    }
-    let oneCount = 0;
-    const n = Math.floor(Math.log2(i));
-    for (let j = 0; j < n; j++) {
-        oneCount += (i & (1<<j))
-    }
-
-    if (oneCount === (size - 1)) {
-        count++;
-    }
-}
-
-console.log(count);
