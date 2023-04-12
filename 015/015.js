d7d199555a79f549884a1bc5ae4a21687cb50d44
Oliver Radini <orr2@live.com>
2023-04-12 21:05:55 +0100

    Add completed version of 15

--- 015/015.js
+++ 015/015.js
@@ -1,10 +1,51 @@
-const { pascalsTriangle } = require("../general/pascalsTriangle");
+const size = 21;
 
-const size = 20;
-const rowsTest = pascalsTriangle((2 * size) - 1);
+const grid = [];
 
-const lastRow = rowsTest[rowsTest.length - 1];
+for (let i = 0; i < size; i++) {
+    grid.push([]);
 
-console.log(lastRow);
+    for (let j = 0; j < size; j++) {
+        grid[i].push(0);
+    }
+}
 
-console.log(Math.max(...lastRow));
+grid[size - 1][size - 1] = 1;
+
+function getNextCoords (x, y) {
+    const points = [];
+    if (x < size - 1) {
+        points.push([x + 1, y]);
+    }
+
+    if (y < size - 1) {
+        points.push([x, y + 1]);
+    }
+
+    return points;
+}
+
+function walkGrid() {
+    for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+            if (grid[y][x] !== 0) {
+                continue;
+            }
+
+            const nextPoints = getNextCoords(x, y);
+
+            let pointTotal = 0;
+            for (let point of nextPoints) {
+                pointTotal += grid[point[1]][point[0]];
+            }
+
+            grid[y][x] = pointTotal;
+        }
+    }
+}
+
+while(grid[0][0] === 0) {
+    walkGrid();
+}
+
+console.log(grid[0][0]);
