15709c9c01320446c1c4335f29048547dde17637
Oliver Radini <orr2@live.com>
2023-05-18 21:52:57 +0100

    First parts of problem 18 written

---
+++ 018/018.js
@@ -0,0 +1,37 @@
+const { readFileSync } = require("fs");
+
+const fileName = process.argv[2] === "2" ? "data2" : "data";
+
+const data = readFileSync(`./018/${fileName}`, "utf8")
+    .split("\n").map(x => x.split(" ").map(Number));
+
+
+function getTriangleFromPosition (row, index) {
+    return [
+        data[row-1][index-1],
+        data[row][index-1],
+        data[row][index],
+    ];
+}
+
+
+function triangleToValue (top, left, right) {
+    return [
+        left > right ? "L" : "R",
+        left > right ? top + left : top + right
+    ];
+}
+
+
+for (let rowIndex = data.length-1; rowIndex > 0; rowIndex--) {
+    const thisRow = data[rowIndex];
+
+    for (let position = 1; position < thisRow.length; position++) {
+        const thisTriangle = getTriangleFromPosition(rowIndex, position);
+    
+        const thisValue = triangleToValue(...thisTriangle);
+        console.log(thisValue);
+    }
+
+    console.log("\n");
+}
