b35a41033f69f25e7d9241d987bed642227ce27f
Oliver Radini <orr2@live.com>
2023-06-20 22:58:22 +0100

    Add merge sort function

--- 022/022.js
+++ 022/022.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const { mergeSort } = require("../general/mergeSort");
 
+const result = mergeSort((l, r) => l < r, [1, 5, 7, 9, 1, 3, 10, 12, 13]);
 
 const names = fs
     .readFileSync("./022/0022_names.txt", "utf8")
--- general/mergeSort.js
+++ general/mergeSort.js
@@ -1,4 +1,63 @@
 
-function mergeSort (f, xs) {
+function mergeLists (l, r, f) {
+    let li = 0;
+    let ri = 0;
+    let lx = null;
+    let rx = null;
+    let lMax = l.length;
+    let rMax = r.length;
 
+    const out = [];
+
+    while (li < lMax && ri < rMax) {
+        lx = l[li];
+        rx = r[ri];
+
+        if (f(lx, rx)) {
+            out.push(lx);
+            li++;
+        } else {
+            out.push(rx);
+            ri++;
+        }
+    }
+
+    while (li < lMax) {
+        out.push(l[li]);
+        li++;
+    }
+
+    while (ri < lMax) {
+        out.push(r[ri]);
+        ri++;
+    }
+
+    return out;
 }
+
+function mergeSort (f, xs) {
+    // unsortable cases
+    if (xs.length === 1 || xs.length === 0) {
+        return xs;
+    }
+
+    // split
+    let lists = xs.map(x => [x]);
+
+    // merge lists
+    while (lists.length > 1) {
+        const newLists = [];
+
+        for (let i = 0; i < lists.length - 1; i += 2) {
+            newLists.push(mergeLists(lists[i], lists[i + 1], f));
+        }
+
+        lists = newLists;
+    }
+
+    return lists;
+}
+
+module.exports = {
+   mergeSort 
+};
