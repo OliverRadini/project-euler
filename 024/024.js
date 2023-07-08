3cc81c6bfa56040b5523e0369d01399a5a38e387
Oliver Radini <orr2@live.com>
2023-07-08 22:55:55 +0100

    Add completed problem 24

--- 024/024.js
+++ 024/024.js
@@ -12,10 +12,13 @@ the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 */
 
 const allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// const allNumbers = ["0", "1", "2"];
 const targetLength = allNumbers.length;
 
+const defaultHas = allNumbers.reduce((p, c) => ({ ...p, [c]: false}), {});
+
 function getUnusedNumbers(x) {
-    const has = { "0": false, "1": false, "2": false, "3": false, "4": false, "5": false, "6": false, "7": false, "8": false, "9": false };
+    const has = { ...defaultHas };
 
     for (let i = 0; i < x.length; i++) {
         delete has[x[i]];
@@ -24,10 +27,25 @@ function getUnusedNumbers(x) {
     return Object.keys(has);
 }
 
-function getAllCombinationsFrom(x) {
+function getAllCombinationsFrom(n) {
+    const unusedNumbers = getUnusedNumbers(n);
 
+    if (unusedNumbers.length === 0) {
+        return [n];
+    }
+
+    const withOneExtraNumber = unusedNumbers
+        .flatMap(x => getAllCombinationsFrom(`${n}${x}`));
+
+    return withOneExtraNumber;
 }
 
-const test = getUnusedNumbers("123");
+const combinations = getAllCombinationsFrom("");
+
+console.log("Generating combinations done");
+
+const sorted = combinations.sort();
+
+const answer = sorted[999999];
 
 const a = 1;
