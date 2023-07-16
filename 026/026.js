dc89df60c382d5579e961bddd7be669ad3761c30
Oliver Radini <orr2@live.com>
2023-07-16 22:41:37 +0100

    Add basic files for problem 26

---
+++ 026/026.js
@@ -0,0 +1,12 @@
+
+const max = 1000;
+
+const numberToDivide = Math.pow(10, 100);
+
+console.log(numberToDivide/7);
+
+// for (let i = 0; i < max; i++) {
+//     const x = numberToDivide / i;
+
+//     const 
+// }
