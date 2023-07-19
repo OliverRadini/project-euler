7e796d1638ee94a8aba565c3f0bc6e9f6fb77549
Oliver Radini <orr2@live.com>
2023-07-19 23:14:46 +0100

    Add basic code for comparing patterns in decimals

--- 026/026.js
+++ 026/026.js
@@ -1,3 +1,4 @@
+const { chunk } = require("../general/chunk");
 
 const maxDenominator = BigInt(1000);
 
@@ -11,7 +12,32 @@ function countPatternSize (n) {
     const s = String(n).replace(".", "");
 }
 
+const maxRepeatingPattern = 499;
+
 for (let d = BigInt(2); d < maxDenominator; d++) {
-    const decimal = numerator / d;
-    console.log(decimal);
+    const decimal = String(numerator / d);
+
+    for (let i = 2; i < maxRepeatingPattern; i++) {
+        const theseChunks = chunk(i, decimal);
+
+        const compareChunk = String(theseChunks[0]);
+
+        const allEqual = theseChunks.reduce(
+            (p, c) => p && String(c) === compareChunk,
+            true,
+        );
+
+        if (allEqual) {
+            console.log(`1/${d}`);
+        }
+    }
+
+    // const max
+
+    // for (let i = 0; i < )
+
+    // for every length up to l / 2
+    // compare the patterns of each size
+    // see if the pattern is repeated throughout
 }
+
